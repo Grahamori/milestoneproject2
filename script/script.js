b06679db7af8f19a2f0b5669eba3e8b7a4c4d26f
Grahamori <grahamori@yahoo.co.uk>
2022-09-09 09:48:10 +0000

    Added some basic script tasks and edit id's

---
+++ script/script.js
@@ -0,0 +1,6 @@
+/* Task One Allow player and Computer to make choices */
+/* Task Two Display those above choices in the results section */
+
+const playerResultDisplay = document.getElementsByClassName('player-choice');
+const computerResultDisplay = document.getElementsByClassName('computer-choice');
+const resultDisplay = document.getElementsByID('player-result');
