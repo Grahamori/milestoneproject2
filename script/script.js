d39c67724c99417f1e15431945dbcd9a5c917b9e
Grahamori <grahamori@yahoo.co.uk>
2022-09-09 15:34:32 +0000

    Removed script code to start again

--- script/script.js
+++ script/script.js
@@ -1,10 +1,5 @@
 /* Task One Allow player and Computer to make choices */
 /* Task Two Display those above choices in the results section */
 
-const playerResultDisplay = document.getElementsByClassName('player-choice');
-const computerResultDisplay = document.getElementsByClassName('computer-choice');
-const resultDisplay = document.getElementsByID('player-result');
-const possibleChoices = document.getElementsByClassName('button');
-
 
 
