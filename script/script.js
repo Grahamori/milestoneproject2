afa8d2900b735d107465a009f6a6f16f218ef8a1
Grahamori <grahamori@yahoo.co.uk>
2022-09-10 10:05:33 +0000

    Added selection array and function

--- script/script.js
+++ script/script.js
@@ -3,14 +3,32 @@
 
 const selectedChoices = document.querySelectorAll('[data-selection]');
 
+const possibleSelections = [
+    {
+        name: 'water',
+        symbol: '🌊',
+        this_beats: 'fire'
+    },
+    {
+        name: 'fire',
+        symbol: '🔥',
+        this_beats: 'earth'
+    },
+    {
+        name: 'earth',
+        symbol: '🌱',
+        this_beats: 'water'
+    }
+];
 
 selectedChoices.forEach(selectedChoice => {
     selectedChoice.addEventListener('click', e => {
         const selectedName = selectedChoice.dataset.selection
-        makeSelection(selectedName)
+        const selection = possibleSelections.find(possibleSelection => possibleSelection.name === selectedName) /* took me a few tries to get this one right */
+        makeSelection(selection)
     })
-});
+})
 
 function makeSelection(selection) {
     console.log(selection) /* Used this to check function works correctly */
-};
+}
