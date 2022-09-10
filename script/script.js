b3f237904e8d8670ba7d48153da0379c8dda3759
Grahamori <grahamori@yahoo.co.uk>
2022-09-10 09:49:12 +0000

    Added function to select a choice

--- script/script.js
+++ script/script.js
@@ -1,5 +1,16 @@
 /* Task One Allow player and Computer to make choices */
 /* Task Two Display those above choices in the results section */
 
+const selectedChoices = document.querySelectorAll('[data-selection]');
 
 
+selectedChoices.forEach(selectedChoice => {
+    selectedChoice.addEventListener('click', e => {
+        const selectedName = selectedChoice.dataset.selection
+        makeSelection(selectedName)
+    })
+});
+
+function makeSelection(selection) {
+    console.log(selection) /* Used this to check function works correctly */
+};
